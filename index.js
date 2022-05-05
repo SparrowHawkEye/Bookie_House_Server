9b327bc68b3787dc87525cd0c1419e76e38a83fa
SparrowHawkEye <makh0043@gmail.com>
2022-05-05 12:18:19 +0600

    CURD opetion Implement

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
@@ -9,20 +10,63 @@ app.use(cors());
 app.use(express.json());
 
 //**CONNECT TO MONGODB */
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wk4vx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 //**CONNECT TO DATABASE */
 
-client.connect(err => {
-  console.log('Database connected');
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const bookCollection = client.db("bookdb").collection("books");
+
+    //** GET API (http://localhost:5000/books) FROM DATABASE*/
+
+    app.get("/books", async (req, res) => {
+      const query = {};
+      const cursor = bookCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //** POST API (http://localhost:5000/book) FROM DATABASE*/
+
+    app.post("/book", async (req, res) => {
+      const book = req.body;
+      const result = await bookCollection.insertOne(book);
+      res.send(result);
+    });
+
+    //** UPDATE API (http://localhost:5000/book/${id}) FROM DATABASE*/
+    app.put("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: { ...data },
+      };
+      const result = await bookCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    //** DELETE API (http://localhost:5000/book/${id}) FROM DATABASE*/
+    app.delete("/note/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await bookCollection.deleteOne(filter);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 //** GET API */
+
 app.get("/", (req, res) => {
   res.send("Running the Sparrow Warehouse Server");
 });
