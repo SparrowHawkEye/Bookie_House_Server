94a2cf25100db0e54be63ebfe62bbdd6d37cfae9
SparrowHawkEye <makh0043@gmail.com>
2023-02-16 00:41:45 +0600

    vercel json added

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ require("dotenv").config();
 const app = express();
 
 //**USE MIDDLEWARE */
-app.use(cors());
+app.use(cors({
+  origin: "https://bookie-house-server.vercel.app/",
+}));
 app.use(express.json());
 
 //**JWT TOKEN */
